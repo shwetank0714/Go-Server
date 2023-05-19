fca2cea4a56761eda8cd609e0eb5752ce923621e
shwetank0714 <shwetank.dwivedi@rara.delivery>
2023-05-19 23:49:48 +0530

    handler functions for a basic server

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	if r.URL.Path != "/form" {
+		http.Error(w, "404 not Found", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "POST request succesful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s \n", name)
+	fmt.Fprintf(w, "Address = %s \n", address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not Found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotAcceptable)
+		return
+	}
+
+	fmt.Fprintf(w, "hello")
+}
+
+func main() {
+
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Print("Starting the server at port 8080\n")
+	if err := http.ListenAndServe(":8080",nil); err != nil {
+		log.Fatal(err)
+	}
+}
